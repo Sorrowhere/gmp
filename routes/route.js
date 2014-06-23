de3489f6a12c0e0c828b1f0411c5c419ddb29f83
2eron <lewiiwel@gmail.com>
2014-06-23 17:30:30 +0800

    write file

--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,7 @@ var marked = require('marked');
 var renderer = new marked.Renderer();
 var hljs = require('highlight.js');
 var fs = require('fs');
+var path = require('path');
 
 marked.setOptions({
 	langPrefix: 'hljs ',
@@ -33,12 +34,7 @@ function route(app){
   			var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
 			return '<h' + level + '><a name="' + escapedText + '" class="anchor" href="#' + escapedText + '">' + text + '</a></h' + level + '>';
 		}
-		// code renderer
-		// renderer.code = function(text){
-		// 	return '<pre><code class="hljs">' + text + '</code></pre>';
-		// }
 
-		// console.log(renderer);
 
 		res.json({
 			content: marked(markContent, { renderer: renderer })
@@ -47,8 +43,21 @@ function route(app){
 
 	// save page
 	router.post('/page', function(req, res){
-		var markContent = req.body.htmlContent;
-		console.log(markContent);
+		var htmlContent = req.body.htmlContent;
+		var	pageName = req.body.pageName;
+		var	folderPath = path.join(__dirname, '../doc/');
+		var	tempHeader = fs.readFileSync(folderPath + 'layout/header.html', 'utf8');
+		var	tempFooter = fs.readFileSync(folderPath + 'layout/footer.html', 'utf8');
+		var tempFile = tempHeader + htmlContent + tempFooter;
+
+		var tempFilePath = folderPath + pageName + '.html';
+
+		// if a file is existed
+		// var fileExisted = fs.existsSync(path);
+
+		fs.writeFileSync(path.join(folderPath + pageName + '.html'), tempFile, 'utf8');
+
+		// console.log(marked);
 	});
 
 
